992eaf7be4118919a547d5f7f8840df30e9d60c8
zhangS <734351162@qq.com>
2020-04-30 01:02:10 +0800

    1.整合了mybatis，新增service,mapper包并写了测试请求。 2.test请求测试已通过。

---
+++ system/src/main/java/com/course/system/domain/Test.java
@@ -0,0 +1,25 @@
+package com.course.system.domain;
+
+import org.springframework.stereotype.Component;
+
+
+public class Test {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
