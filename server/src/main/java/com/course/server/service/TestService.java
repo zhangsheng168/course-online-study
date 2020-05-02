6e8fd63802fe8f7d6ba283bd117796a82329cfcc
zhangS <734351162@qq.com>
2020-05-02 23:00:43 +0800

    TestExample的使用实例

--- server/src/main/java/com/course/server/service/TestService.java
+++ server/src/main/java/com/course/server/service/TestService.java
@@ -1,6 +1,7 @@
 package com.course.server.service;
 
 import com.course.server.domain.Test;
+import com.course.server.domain.TestExample;
 import com.course.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,11 @@ public class TestService {
     private TestMapper testMapper;
 
     public List<Test> list(){
-        return testMapper.selectByExample(null);
+        TestExample te = new TestExample();
+        //等同于where条件
+        te.createCriteria().andIdEqualTo("1");
+        te.setOrderByClause("id desc");
+
+        return testMapper.selectByExample(te);
     }
 }
