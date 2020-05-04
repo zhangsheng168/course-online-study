8e6aeeee44cd7360b19ef36b1588d28b9d1dd283
zhangS <734351162@qq.com>
2020-05-05 00:43:13 +0800

    1.调整了侧边栏与底部。删除了暂时不需要的菜单。

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -16,7 +16,7 @@
                     <a href="index.html" class="navbar-brand">
                         <small>
                             <i class="fa fa-leaf"></i>
-                            Ace Admin
+                            产教融合在线学习平台
                         </small>
                     </a>
                 </div>
@@ -365,7 +365,7 @@
                         <b class="arrow"></b>
                     </li>
 
-                    <li class="">
+                   <!-- <li class="">
                         <a href="#" class="dropdown-toggle">
                             <i class="menu-icon fa fa-desktop"></i>
                             <span class="menu-text">
@@ -561,12 +561,12 @@
                                 </ul>
                             </li>
                         </ul>
-                    </li>
+                    </li>-->
 
-                    <li class="">
+                   <li class="active open">
                         <a href="#" class="dropdown-toggle">
                             <i class="menu-icon fa fa-list"></i>
-                            <span class="menu-text"> Tables </span>
+                            <span class="menu-text"> 系统管理 </span>
 
                             <b class="arrow fa fa-angle-down"></b>
                         </a>
@@ -577,7 +577,7 @@
                             <li class="">
                                 <a href="tables.html">
                                     <i class="menu-icon fa fa-caret-right"></i>
-                                    Simple &amp; Dynamic
+                                   用户管理
                                 </a>
 
                                 <b class="arrow"></b>
@@ -586,7 +586,7 @@
                             <li class="">
                                 <a href="jqgrid.html">
                                     <i class="menu-icon fa fa-caret-right"></i>
-                                    jqGrid plugin
+                                    权限管理
                                 </a>
 
                                 <b class="arrow"></b>
@@ -594,7 +594,7 @@
                         </ul>
                     </li>
 
-                    <li class="">
+                   <!-- <li class="">
                         <a href="#" class="dropdown-toggle">
                             <i class="menu-icon fa fa-pencil-square-o"></i>
                             <span class="menu-text"> Forms </span>
@@ -770,8 +770,8 @@
                             </li>
                         </ul>
                     </li>
-
-                    <li class="active open">
+-->
+                   <!-- <li class="active open">
                         <a href="#" class="dropdown-toggle">
                             <i class="menu-icon fa fa-file-o"></i>
 
@@ -832,7 +832,7 @@
                                 <b class="arrow"></b>
                             </li>
                         </ul>
-                    </li>
+                    </li>-->
                 </ul><!-- /.nav-list -->
 
                 <div class="sidebar-toggle sidebar-collapse" id="sidebar-collapse">
@@ -842,96 +842,9 @@
 
             <div class="main-content">
                 <div class="main-content-inner">
-                    <!--<div class="breadcrumbs ace-save-state" id="breadcrumbs">
-                        <ul class="breadcrumb">
-                            <li>
-                                <i class="ace-icon fa fa-home home-icon"></i>
-                                <a href="#">Home</a>
-                            </li>
 
-                            <li>
-                                <a href="#">Other Pages</a>
-                            </li>
-                            <li class="active">Blank Page</li>
-                        </ul>&lt;!&ndash; /.breadcrumb &ndash;&gt;
-
-                        <div class="nav-search" id="nav-search">
-                            <form class="form-search">
-								<span class="input-icon">
-									<input type="text" placeholder="Search ..." class="nav-search-input" id="nav-search-input" autocomplete="off" />
-									<i class="ace-icon fa fa-search nav-search-icon"></i>
-								</span>
-                            </form>
-                        </div>&lt;!&ndash; /.nav-search &ndash;&gt;
-                    </div>
--->
                     <div class="page-content">
-                        <!--<div class="ace-settings-container" id="ace-settings-container">
-                            <div class="btn btn-app btn-xs btn-warning ace-settings-btn" id="ace-settings-btn">
-                                <i class="ace-icon fa fa-cog bigger-130"></i>
-                            </div>
 
-                            <div class="ace-settings-box clearfix" id="ace-settings-box">
-                                <div class="pull-left width-50">
-                                    <div class="ace-settings-item">
-                                        <div class="pull-left">
-                                            <select id="skin-colorpicker" class="hide">
-                                                <option data-skin="no-skin" value="#438EB9">#438EB9</option>
-                                                <option data-skin="skin-1" value="#222A2D">#222A2D</option>
-                                                <option data-skin="skin-2" value="#C6487E">#C6487E</option>
-                                                <option data-skin="skin-3" value="#D0D0D0">#D0D0D0</option>
-                                            </select>
-                                        </div>
-                                        <span>&nbsp; Choose Skin</span>
-                                    </div>
-
-                                    <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2 ace-save-state" id="ace-settings-navbar" autocomplete="off" />
-                                        <label class="lbl" for="ace-settings-navbar"> Fixed Navbar</label>
-                                    </div>
-
-                                    <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2 ace-save-state" id="ace-settings-sidebar" autocomplete="off" />
-                                        <label class="lbl" for="ace-settings-sidebar"> Fixed Sidebar</label>
-                                    </div>
-
-                                    <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2 ace-save-state" id="ace-settings-breadcrumbs" autocomplete="off" />
-                                        <label class="lbl" for="ace-settings-breadcrumbs"> Fixed Breadcrumbs</label>
-                                    </div>
-
-                                    <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-rtl" autocomplete="off" />
-                                        <label class="lbl" for="ace-settings-rtl"> Right To Left (rtl)</label>
-                                    </div>
-
-                                    <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2 ace-save-state" id="ace-settings-add-container" autocomplete="off" />
-                                        <label class="lbl" for="ace-settings-add-container">
-                                            Inside
-                                            <b>.container</b>
-                                        </label>
-                                    </div>
-                                </div>&lt;!&ndash; /.pull-left &ndash;&gt;
-
-                                <div class="pull-left width-50">
-                                    <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-hover" autocomplete="off" />
-                                        <label class="lbl" for="ace-settings-hover"> Submenu on Hover</label>
-                                    </div>
-
-                                    <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-compact" autocomplete="off" />
-                                        <label class="lbl" for="ace-settings-compact"> Compact Sidebar</label>
-                                    </div>
-
-                                    <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-highlight" autocomplete="off" />
-                                        <label class="lbl" for="ace-settings-highlight"> Alt. Active Item</label>
-                                    </div>
-                                </div>&lt;!&ndash; /.pull-left &ndash;&gt;
-                            </div>&lt;!&ndash; /.ace-settings-box &ndash;&gt;
-                        </div>--><!-- /.ace-settings-container -->
 
                         <div class="row">
                             <div class="col-xs-12">
@@ -948,24 +861,12 @@
                 <div class="footer-inner">
                     <div class="footer-content">
 						<span class="bigger-120">
-							<span class="blue bolder">Ace</span>
+							<span class="blue bolder">产教融合在线学习平台</span>
 							Application &copy; 2013-2014
 						</span>
 
                         &nbsp; &nbsp;
-                        <span class="action-buttons">
-							<a href="#">
-								<i class="ace-icon fa fa-twitter-square light-blue bigger-150"></i>
-							</a>
 
-							<a href="#">
-								<i class="ace-icon fa fa-facebook-square text-primary bigger-150"></i>
-							</a>
-
-							<a href="#">
-								<i class="ace-icon fa fa-rss-square orange bigger-150"></i>
-							</a>
-						</span>
                     </div>
                 </div>
             </div>
