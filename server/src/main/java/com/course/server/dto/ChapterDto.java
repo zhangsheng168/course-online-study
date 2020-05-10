cd222c6df9afdbf25e9191c4c9d09eeaeb02dde1
zhangS <734351162@qq.com>
2020-05-10 10:59:53 +0800

    1.增加了dto层，主要用于controller跟service 2.增加了ChapterDto 3.修改了ChapterService,将返回的Chapter改成返回ChapterDto

---
+++ server/src/main/java/com/course/server/dto/ChapterDto.java
@@ -0,0 +1,53 @@
+package com.course.server.dto;
+
+/**
+ * domain是由generatorConfig自动生成的，不允许修改。当再次执行生成会覆盖原来的文件
+ * dto可以进行修改。
+ *
+ * domain作用于service和mapper
+ * dto作用于controller和service
+ */
+public class ChapterDto {
+    private String id;
+
+    private String courseId;
+
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", courseId=").append(courseId);
+        sb.append(", name=").append(name);
+        sb.append("]");
+        return sb.toString();
+    }
+}
