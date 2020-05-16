681d7f7d525b18348707a789797ac8ec25a68e17
zhangS <734351162@qq.com>
2020-05-16 16:33:57 +0800

    1.分页参数前后端交互，axios的post请求默认是以流的方式传递参数，所以controller里的参数要加@RequestBody注解

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -3,8 +3,12 @@ package com.course.business.controller.admin;
 import com.course.server.domain.Chapter;
 import com.course.server.domain.Chapter;
 import com.course.server.dto.ChapterDto;
+import com.course.server.dto.PageDto;
 import com.course.server.service.ChapterService;
 import com.course.server.service.ChapterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +19,15 @@ import java.util.List;
 @RequestMapping("/admin/chapter")
 public class ChapterController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ChapterController.class);
+
     @Resource
     private ChapterService chapterService;
 
     @RequestMapping("/list")
-    public List<ChapterDto> list(){
-        System.out.println("----------1111-------");
-        return chapterService.list();
+    public PageDto list(@RequestBody PageDto pageDto){
+        LOG.info("pageDto:{}",pageDto);
+        chapterService.list(pageDto);
+        return pageDto;
     }
 }
--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -3,8 +3,10 @@ package com.course.server.service;
 import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
 import com.course.server.dto.ChapterDto;
+import com.course.server.dto.PageDto;
 import com.course.server.mapper.ChapterMapper;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -18,15 +20,18 @@ public class ChapterService {
     @Resource
     private ChapterMapper ChapterMapper;
 
-    public List<ChapterDto> list(){
+    public void list(PageDto pageDto){
         //分页（该插件往下遇到的第一个selectXX则生效）
-        PageHelper.startPage(1,1);
-        ChapterExample chapterExample = new ChapterExample();
+        PageHelper.startPage(pageDto.getPage() ,pageDto.getSize());
         //等同于where条件
        /* chapterExample.createCriteria().andIdEqualTo("1");
         chapterExample.setOrderByClause("id desc");*/
-
+        ChapterExample chapterExample = new ChapterExample();
         List<Chapter> chapterList = ChapterMapper.selectByExample(chapterExample);
+        PageInfo<Chapter> pageInfo = new PageInfo<>(chapterList);
+        //设置总行数
+        pageDto.setTotal(pageInfo.getTotal());
+
         List<ChapterDto> chapterDtoList = new ArrayList<ChapterDto>();
         for (int i = 0,l=chapterList.size(); i<l ; i++) {
             Chapter chapter = chapterList.get(i);
@@ -34,7 +39,6 @@ public class ChapterService {
             BeanUtils.copyProperties(chapter,chapterDto);
             chapterDtoList.add(chapterDto);
         }
-
-        return chapterDtoList;
+        pageDto.setList(chapterDtoList);
     }
 }
